85773309009a1af4c9a3b9c8e83d77e8aabb077b
Subaru Sakaguchi <subarusakaguchi@yahoo.com.br>
2022-07-08 14:16:42 -0300

    Basic project created

---
+++ src/shared/http/app.ts
@@ -0,0 +1,25 @@
+import express, { NextFunction, Request, Response } from "express";
+
+import "express-async-errors";
+import { AppError } from "../errors/App.Error";
+import { router } from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use("/api/v1", router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: "Error",
+    message: `Internal server error - ${err.message}`,
+  });
+});
+
+export { app };
